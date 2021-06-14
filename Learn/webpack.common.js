36cf38437f210c173a044da497787a311ad3970a
adityanegara <aditnegarabisnis@gmail.com>
2021-06-15 00:27:02 +0700

    Learn : HTML-loader and Clean Webpack

--- Learn/webpack.common.js
+++ Learn/webpack.common.js
@@ -10,7 +10,18 @@
              {
                test: /\.s[ac]ss$/i,
                use : ["style-loader" ,"css-loader", "sass-loader"]
+             },
+             {
+          
+               test : /\.html$/,
+               loader : 'html-loader',
+             },
+             {
+               test: /\.(svg|png|jpg|gif)/,
+               type : 'asset/resource'
              }
+            
+             
         ]
      }
  }
--- Learn/webpack.prod.js
+++ Learn/webpack.prod.js
@@ -1,10 +1,14 @@
 const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = merge(common,{
     mode : "production",
     output: {
        filename : 'main.[contenthash].js',
-       path : path.resolve(__dirname, "dist")
+       path : path.resolve(__dirname, "dist"),
+       assetModuleFilename: 'images/[name].[hash][ext]'
     },
+    plugins: [ new CleanWebpackPlugin()]
 });

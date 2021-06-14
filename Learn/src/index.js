ffc301260957468553f23574679859413295981b
adityanegara <aditnegarabisnis@gmail.com>
2021-06-14 14:27:34 +0700

    Learn : Cache Busting and Plugins

--- Learn/src/index.js
+++ Learn/src/index.js
@@ -3,6 +3,7 @@ import "./main.scss";
 import {AlertService} from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);

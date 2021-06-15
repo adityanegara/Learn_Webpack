0799479aee7a766b132432a360f0256e16a11a3a
adityanegara <aditnegarabisnis@gmail.com>
2021-06-15 11:41:35 +0700

    Practice : html loader, clean webpack

--- Practice/src/index.js
+++ Practice/src/index.js
@@ -1,3 +1,4 @@
 import "./style.scss";
 import {main} from './app/main.js'
+console.log("aditya negara");
 main();
--- Practice/webpack.common.js
+++ Practice/webpack.common.js
@@ -10,7 +10,17 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use : ["style-loader" ,"css-loader", "sass-loader"]
+            },
+            {
+                test : /\.html$/,
+                loader : 'html-loader'
+            },
+            {
+                test: /\.(svg|png|jpg|gif)/,
+                type : 'asset/resource'
             }
+           
+
          ]
     }
 }

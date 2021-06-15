3b689cc343ee7550dec8411388af36203f223646
adityanegara <aditnegarabisnis@gmail.com>
2021-06-15 13:01:51 +0700

    Learn : Extract CSS & Minify

--- Learn/webpack.prod.js
+++ Learn/webpack.prod.js
@@ -2,6 +2,9 @@ const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common,{
     mode : "production",
@@ -10,5 +13,18 @@ module.exports = merge(common,{
        path : path.resolve(__dirname, "dist"),
        assetModuleFilename: 'images/[name].[hash][ext]'
     },
-    plugins: [ new CleanWebpackPlugin()]
+    plugins: [new MiniCssExtractPlugin({filename: '[name].[contenthash].css'}), new CleanWebpackPlugin()],
+    module : {
+        rules : [
+            {
+                test: /\.s[ac]ss$/i,
+                use : [MiniCssExtractPlugin.loader ,"css-loader", "sass-loader"]
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+          new CssMinimizerPlugin(),new TerserPlugin()
+        ],
+      },
 });
